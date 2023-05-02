b3cb3cd626e058f4e4c99f5b746bf8fe60f52a71
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-02 17:02:24 +0200

    add: header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script>
+import axios from "axios";
+import {store} from "./data/store.js";
+import Header from "./components/Header.vue";
 export default{
-  name: "App"
+  name: "App",
+  components:{
+    Header,
+  },
+  data(){
+    return{
+      store
+    }
+  }
 }
 </script>
 
 <template>
-
+  <Header/>
 </template>
 
 <style lang="scss">
-
+@import "./scss/main.scss";
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<template>
+  <header class="bg-white">
+    <div class="container h-100 d-flex align-items-center">
+      <img src="/yugioh.svg" class="h-25 me-3" alt="Logo Yu-Gi-Oh">
+      <h1>Vite Yu-Gi-Oh! API</h1>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+  header{
+    height: 100px;
+  }
+</style>
