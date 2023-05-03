e5139901fb10f56247372f71aef27a504e5248a2
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-03 18:22:42 +0200

    add: listType and newType

--- src/data/store.js
+++ src/data/store.js
@@ -4,6 +4,8 @@ export const store = reactive({
   apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php",
   resultArrayTotal: [],
   printArray: [],
+  listType: [],
+  newType: null,
   cardNumber: 20,
   cardOffset: 0,
   isLoading: true
