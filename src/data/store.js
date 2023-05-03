7284636a0def9440de502542a1bdad613d197b93
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-05-03 17:33:28 +0200

    add: esultArrayTotal and printArray

--- src/data/store.js
+++ src/data/store.js
@@ -2,8 +2,9 @@ import {reactive} from "vue";
 
 export const store = reactive({
   apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php",
-  resultArray: [],
-  cardNumber: 10,
+  resultArrayTotal: [],
+  printArray: [],
+  cardNumber: 20,
   cardOffset: 0,
   isLoading: true
 })
